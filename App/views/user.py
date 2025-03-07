015db8b01e72519c17847fd55370e2725b5cf66b
matt8142 <157655070+matt8142@users.noreply.github.com>
2025-03-07 18:13:32 +0000

    user commit

--- App/views/user.py
+++ App/views/user.py
@@ -1,21 +1,19 @@
 from flask import Blueprint, render_template, jsonify, request, send_from_directory, flash, redirect, url_for
 from flask_jwt_extended import jwt_required, current_user as jwt_current_user
 
-from.index import index_views
+from .index import index_views
 
-from App.controllers import (
-    create_user,
-    get_all_users,
-    get_all_users_json,
-    jwt_required
-)
+from App.controllers import (create_user, get_all_users, get_all_users_json,
+                             jwt_required)
 
 user_views = Blueprint('user_views', __name__, template_folder='../templates')
+
 
 @user_views.route('/users', methods=['GET'])
 def get_user_page():
     users = get_all_users()
     return render_template('users.html', users=users)
+
 
 @user_views.route('/users', methods=['POST'])
 def create_user_action():
@@ -24,17 +22,27 @@ def create_user_action():
     create_user(data['username'], data['password'])
     return redirect(url_for('user_views.get_user_page'))
 
+
 @user_views.route('/api/users', methods=['GET'])
 def get_users_action():
     users = get_all_users_json()
     return jsonify(users)
 
+
 @user_views.route('/api/users', methods=['POST'])
 def create_user_endpoint():
     data = request.json
     user = create_user(data['username'], data['password'])
-    return jsonify({'message': f"user {user.username} created with id {user.id}"})
+    return jsonify(
+        {'message': f"user {user.username} created with id {user.id}"})
+
 
 @user_views.route('/static/users', methods=['GET'])
 def static_user_page():
-  return send_from_directory('static', 'static-user.html')
+    return send_from_directory('static', 'static-user.html')
+
+
+@user_views.route('/api/users')
+def client_app():
+    users = get_all_users_json()
+    return jsonify(users)
